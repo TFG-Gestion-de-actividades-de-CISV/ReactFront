552e7604492d2a77de62b1b8cc41b7c58ac7b0a6
VladUPC <vladyslav.barzion@estudiantat.upc.edu>
2024-04-29 13:39:34 +0200

    Error resuelto: dos llamadas a api

--- src/main.jsx
+++ src/main.jsx
@@ -9,14 +9,12 @@ import AdminMainView from "./Views/AdminMainView";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainPageView />} />
-        <Route path="/login" element={<LoginView />} />
-        <Route path="/signup" element={<SignupView />} />
-        <Route path="/admin/main" element={<AdminMainView />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<MainPageView />} />
+      <Route path="/login" element={<LoginView />} />
+      <Route path="/signup" element={<SignupView />} />
+      <Route path="/admin/main" element={<AdminMainView />} />
+    </Routes>
+  </Router>
 );
