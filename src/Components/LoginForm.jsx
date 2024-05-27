063dcea5e5e1fee061430eee875a23520978c5f1
VladUPC <vladyslav.barzion@estudiantat.upc.edu>
2024-05-27 03:20:09 +0200

    POrfavor

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -21,7 +21,7 @@ const LoginForm = ({ onLogin }) => {
     handleSubmit,
   } = useForm();
 
-  const url = "http://172.26.8.5:8000/web_user/login/";
+  const url = "http:/http://15.237.251.169/web_user/login/";
 
   function onSubmit(data) {
     fetch(url, {
