0d995e5e63fbc3a9566869aec82ac5e3ca15e15b
VladUPC <vladyslav.barzion@estudiantat.upc.edu>
2024-06-04 15:46:16 +0200

    Autorizacion de usar imagenes

--- src/Components/NinosInscriptionForm.jsx
+++ src/Components/NinosInscriptionForm.jsx
@@ -10,6 +10,7 @@ import {
 } from "@mui/material";
 import { useForm, Controller } from "react-hook-form";
 import config from "../config";
+import CisvConditionsDialog from "./CisvConditionsDialog";
 
 const NinosInscriptionForm = ({ activity }) => {
   const {
@@ -22,12 +23,12 @@ const NinosInscriptionForm = ({ activity }) => {
   } = useForm({
     defaultValues: {
       allergy: "",
-      cisv_authorization: false,
       emergency_phone: "",
       t_shirt_size: "",
       medicines: "",
       health_card: null,
       pago: null,
+      image_authorization: false,
     },
   });
 
@@ -35,6 +36,7 @@ const NinosInscriptionForm = ({ activity }) => {
   const [successMessage, setSuccessMessage] = useState(null);
   const [healthCardUrl, setHealthCardUrl] = useState(null);
   const [pagoUrl, setPagoUrl] = useState(null);
+  const [cisvAutorization, setCisvAutorization] = useState(false);
 
   const url = `${config.apiUrl}/activities/ninos_inscription/`;
   const getOrCreateUrl = `${config.apiUrl}/activities/get_or_create_inscription/ninos`;
@@ -175,7 +177,12 @@ const NinosInscriptionForm = ({ activity }) => {
 
           <Grid item xs={12} md={6}>
             <Typography> Tarjeta Sanitaria</Typography>
-            <input type="file" accept=".pdf" {...register("health_card")} />
+            <input
+              type="file"
+              required
+              accept=".pdf"
+              {...register("health_card")}
+            />
             {healthCardUrl && (
               <div>
                 <a
@@ -192,7 +199,7 @@ const NinosInscriptionForm = ({ activity }) => {
           <Grid item xs={12} md={6}>
             <Typography> Comprobante de pago</Typography>
 
-            <input type="file" accept=".pdf" {...register("pago")} />
+            <input type="file" required accept=".pdf" {...register("pago")} />
             {pagoUrl && (
               <div>
                 <a href={pagoUrl} target="_blank" rel="noopener noreferrer">
@@ -203,10 +210,17 @@ const NinosInscriptionForm = ({ activity }) => {
           </Grid>
 
           <Grid item xs={12} md={6}>
-            <Typography variant="h6">¿Autorización de CISV?</Typography>
+            <CisvConditionsDialog
+              checked={cisvAutorization}
+              onChange={(e) => setCisvAutorization(e.target.checked)}
+            />
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <Typography variant="h6">¿Autoriza usar su imagen?</Typography>
 
             <Controller
-              name="cisv_authorization"
+              name="image_authorization"
               required
               control={control}
               render={({ field }) => (
@@ -234,7 +248,11 @@ const NinosInscriptionForm = ({ activity }) => {
           )}
 
           <Grid item xs={12}>
-            <Button variant="outlined" type="submit">
+            <Button
+              variant="outlined"
+              type="submit"
+              disabled={!cisvAutorization}
+            >
               Inscribirse
             </Button>
           </Grid>
