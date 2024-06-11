675c7d2ab768ac9c99f248b5a25b6bb62b536490
VladUPC <vladyslav.barzion@estudiantat.upc.edu>
2024-06-11 17:47:57 +0200

    Eliminacion de actividad dejando ultima inscripcion de user

--- src/Components/ListActivities.jsx
+++ src/Components/ListActivities.jsx
@@ -5,6 +5,13 @@ import {
   Card,
   CardContent,
   Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Alert,
+  AlertTitle,
+  Grid,
 } from "@mui/material";
 import config from "../config";
 import { useNavigate } from "react-router-dom";
@@ -12,6 +19,10 @@ import { useNavigate } from "react-router-dom";
 const ListActivities = ({ onActivityClick }) => {
   const [activities, setActivities] = useState([]);
   const navigate = useNavigate();
+  const [deleteActivityId, setDeleteActivityId] = useState(null);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
   const isAdmin = localStorage.getItem("isAdmin") === "true";
 
   const url = `${config.apiUrl}/activities/all_activities`;
@@ -28,6 +39,29 @@ const ListActivities = ({ onActivityClick }) => {
 
   const handleEditClick = (activityId) => {
     navigate(`/admin/edit_activity/${activityId}`);
+  };
+
+  const handleDeleteClick = (activityId) => {
+    setDeleteActivityId(activityId);
+    setOpenDialog(true);
+  };
+
+  const handleConfirmDelete = () => {
+    fetch(`${config.apiUrl}/activities/delete_activity/${deleteActivityId}`, {
+      method: "DELETE",
+      credentials: "include",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error al eliminar la actividad");
+        }
+        setSuccessMessage("Actividad eliminada exitosamente");
+        setOpenDialog(false);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        setErrorMessage("Ha ocurrido un error al eliminar la actividad.");
+      });
   };
 
   return (
@@ -71,17 +105,60 @@ const ListActivities = ({ onActivityClick }) => {
               Ver Inscripciones
             </Button>
             {isAdmin && (
-              <Button
-                variant="contained"
-                color="secondary"
-                onClick={() => handleEditClick(activity.id)}
-              >
-                Editar
-              </Button>
+              <>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => handleEditClick(activity.id)}
+                >
+                  Editar
+                </Button>
+
+                <Button
+                  variant="contained"
+                  color="error"
+                  onClick={() => handleDeleteClick(activity.id)}
+                >
+                  Eliminar
+                </Button>
+              </>
             )}
           </CardContent>
         </Card>
       ))}
+      <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
+        <DialogTitle>Confirmar eliminación</DialogTitle>
+        <DialogContent>
+          <Typography>
+            ¿Estás seguro de que deseas eliminar esta actividad?
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpenDialog(false)} color="primary">
+            Cancelar
+          </Button>
+          <Button onClick={handleConfirmDelete} color="error" autoFocus>
+            Eliminar
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      {successMessage && (
+        <Grid item xs={12}>
+          <Alert severity="success">
+            <AlertTitle>Éxito</AlertTitle>
+            {successMessage}
+          </Alert>
+        </Grid>
+      )}
+      {errorMessage && (
+        <Grid item xs={12}>
+          <Alert severity="error">
+            <AlertTitle>Error</AlertTitle>
+            {errorMessage}
+          </Alert>
+        </Grid>
+      )}
     </Container>
   );
 };
